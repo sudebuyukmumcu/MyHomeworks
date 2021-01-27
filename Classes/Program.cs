621d633e7c1aa98b726e385ccab369bf04983d06
sudebuyukmumcu <77405773+sudebuyukmumcu@users.noreply.github.com>
2021-01-27 16:08:58 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //bu şekilde yazarak class ı kullanmak için onun bir örneğini oluşturmak gerekir
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager1 productManager = new ProductManager1();
+            productManager.Add();
+            productManager.Update();
+
+            Console.ReadLine();
+        }
+
+    }
+   
+       
+        }
+    
+
