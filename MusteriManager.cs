b5b22baf0f41f5c5c10202f3fd8222b10fdc1ba4
sudebuyukmumcu <77405773+sudebuyukmumcu@users.noreply.github.com>
2021-01-30 18:16:27 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen Müşteri./n" + musteri.Id + "/n " + musteri.Adi + "/n" + musteri.Soyadi + "/n" + musteri.Yasi + "/n" + musteri.MusteriNo + "/n");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Silinen Müşteri" + musteri.Id + "/n " + musteri.Adi + "/n" + musteri.Soyadi + "/n" + musteri.Yasi + "/n" + musteri.MusteriNo+"/n");
+        }
+       public void List(Musteri musteri)
+        {
+            Console.WriteLine("Listelenen Müşteri" + musteri.Id + "/n " + musteri.Adi + "/n" + musteri.Soyadi + "/n" + musteri.Yasi + "/n" + musteri.MusteriNo + "/n");
+        }
+        
+        
+    }
+}
---
+++ MusteriManagerBase.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassMetotDemo
+{
+    internal class MusteriManagerBase
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen Müşterinin ID'si : " + musteri.Id);
+            Console.WriteLine("Eklenen Müşterinin Adi: " + musteri.Adi);
+            Console.WriteLine("Eklenen Müşterinin Soyadi:" + musteri.Soyadi);
+            Console.WriteLine("Eklenen Müşterinin Yaşı:" + musteri.Yasi);
+            Console.WriteLine("Eklenen Müşterinin Müşteri No'su:" + musteri.MusteriNo);
+        }
+        public void Delete(Musteri musteri)
+    }
+}
