927092ff5fa6de1fc3c28100c00f5a76f681defd
sudebuyukmumcu <77405773+sudebuyukmumcu@users.noreply.github.com>
2021-01-30 17:59:12 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class CustomerManager
+    {
+        //böylece CustomerManager class ımın içinde iki tane metod oluşturdum.
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated");
+        }
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //bu şekilde yazarak class ı kullanmak için onun bir örneğini oluşturmak gerekir
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager1 productManager = new ProductManager1();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demirog";
+            //Bir class ın property sini eşitlediğiniz zaman yani( firstname=engin ) gibi
+            //Bu property deki set blogu çaşılır. o yüzden set blogudur bu.
+   
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,City = "İstanbul",FirstName = "Derin",LastName="Demirog"
+            };
+
+            Console.WriteLine(customer2.FirstName); //burada çalışan ise get blogudur. yani bunu getir.
+            
+            Console.ReadLine();
+        }
+
+    }
+   
+       
+}
+    
+
