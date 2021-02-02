90498c49400c16388d335b1f441f0aa848e98057
sudebuyukmumcu <77405773+sudebuyukmumcu@users.noreply.github.com>
2021-02-02 18:40:18 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReCapDemo1
+{
+    public partial class Form1 : Form
+    {
+        private const int V = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NewMethod();
+
+        }
+
+        private void NewMethod()
+        {
+            //Button button = new Button(); //buton nesnesini oluşturmuş oluyorum
+            //button.Width = 50;
+            //button.Height = 50;
+            //this.Controls.Add(button); //ekrana eklemiş oluyoruz
+            //64 adet butonum olsun istiyorum. Bunu 64 kez yazmak yerine;
+            //döngü yapabilirsin ama üst üste bindirilmiş 64 buton olur. 
+            //butonların her birine ayrı ayr erişmek istiyorsan.kısacası aynı işlemden farklı nesler oluşturup nları daha sonra kontrol altında tutmak istiyorsan;
+            //array yapıyoruz.
+            Button[,] buttons = new Button[8, 8]; //başlangıcım bu
+                                                  //şimdi her satır için 8 e 8 lik buton koyucaz.
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++) //arrayleri genellikle sıfırdan başlatırız
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button(); //her buton bizim için her dfeasında yeni bir butondur. O yüzden her defasında newlememiz gerekir
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50; //sola 50 kaydır dedik. 64 tane butonu yan yana yerleştirir.
+
+                    buttons[i, j].BackColor = (i + j) % 2 == V ? Color.Black : Color.White;
+
+                    this.Controls.Add(buttons[i, j]); //burada this dediğimiz forma karşılık gelir.
+                    //64 kez çalışmış üst üste butonlar koydu. 1. butonun başlangı değeri formun en uç noktası yani sıfır noktası.
+                    //dolayısıyla bir değişken tanımlıcam. int top, int left seklinde for dan önce tanımladık.
+                }
+                //bizim her satır bittiğinde top += 50 demem lazım ki 2. satıra geçtiğide 50 artsın.
+                top += 50;
+                left = 0; //2. satıra geçtiğimde lefti sıfırlamam lazım
+                //böylece 8 e 8 lik buton array i oluştu.
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,42 +1,23 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Classes
+namespace ReCapDemo1
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            CustomerManager customerManager = new CustomerManager();
-            //bu şekilde yazarak class ı kullanmak için onun bir örneğini oluşturmak gerekir
-            customerManager.Add();
-            customerManager.Update();
-
-            ProductManager1 productManager = new ProductManager1();
-            productManager.Add();
-            productManager.Update();
-
-            Customer customer = new Customer();
-            customer.City = "Ankara";
-            customer.Id = 1;
-            customer.FirstName = "Engin";
-            customer.LastName = "Demirog";
-            //Bir class ın property sini eşitlediğiniz zaman yani( firstname=engin ) gibi
-            //Bu property deki set blogu çaşılır. o yüzden set blogudur bu.
-   
-
-            Customer customer2 = new Customer
-            {
-                Id = 2,City = "İstanbul",FirstName = "Derin",LastName="Demirog"
-            };
-
-            Console.WriteLine(customer2.FirstName); //burada çalışan ise get blogudur. yani bunu getir.
-            
-            Console.ReadLine();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
-
     }
-   
-       
 }
-    
-
